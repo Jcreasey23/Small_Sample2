6ae2632772d67bab11eb931404e398e0dff72d97
Angel Ainembabazi <aainembabazi23@my.whitworth.edu>
2022-05-13 19:07:03 -0700

    update cpp file

--- headers/city.h
+++ headers/city.h
@@ -12,7 +12,7 @@ class City{
     int avgTemp; //Average temperature of the city in a given month
     int cityPop; //population of the city
     int numRobbery; //robberies in a given month
-    int gTa; //grand theft auto in a given month
+    int numGtA; //grand theft auto in a given month
     int numMurder; //murder in a given month
     int numTheft; //theft in a given month
     int numVandalism; //trespassing in a given month
@@ -25,11 +25,11 @@ class City{
     
     public:
         City(); 
-        City(string newCityName, int newAvgTemp, int newCityPop, int newNumRobbery, int newGtA, int newNumMurder, int newNumTheft, int newNumVand);
+        City(string newCityName, int newAvgTemp, int newCityPop, int newNumRobbery, int newNumGtA, int newNumMurder, int newNumTheft, int newNumVand);
         string getCityName(); //returns city name
         int getCityPop();//returns city population
         int getNumRobbery();//returns the number of robbery cases
-        int getGtA();//returns the number of grand theft auto cases
+        int getNumGtA();//returns the number of grand theft auto cases
         int getNumMurder(); //returns the number of murder cases
         int getNumTheft(); //returns the number of theft cases
         int getNumVand(); //returns the number of vandalism cases
