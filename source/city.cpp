06387ad6ba553e5468f58b2454cdea6a797c007f
jcreasey23 <jcreasey23@my.whitworth.edu>
2022-05-18 16:54:47 -0700

    Read all files

--- source/city.cpp
+++ source/city.cpp
@@ -45,19 +45,3 @@ int City::getNumTheft(){
 }
 
 
-<<<<<<< HEAD
-// void City::readFile(ifstream &cityFile, vector<City> cityName)
-// {
-//     int counter = 0;
-
-//     while(counter < 12) // ** Problem ** Infinite loop as it reads the last line over again at the end of the file when using (!input.eof())
-//     {
-//         cityFile >> month >> avgTemp >> numMurder >> numGtA >> numRobbery >> numTheft;   // Reads one line from txt file
-//         cityName.push_back(City(avgTemp,numMurder,numGtA,numRobbery,numTheft));   // Adds runner object to vector of runner objects
-//         cout << month << " " << avgTemp << " " << numMurder << " " << numGtA << " " << numRobbery << " " <<  numTheft << endl;  // Prints string of runner first name, last name, and pace per mile in seconds
-//         counter++;
-//     }
-// }
-=======
-
->>>>>>> 6f73395721aedfae5a4497fcdd5160dd636ef368
